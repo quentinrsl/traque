c239ab4487c2e261b5946c9aa191ab0b3aaa83dd
Quentin Roussel <contact@quentinrsl.com>
2024-03-27 22:22:35 +0100

    changed number fields to numeric

--- traque-front/components/team/loginForm.jsx
+++ traque-front/components/team/loginForm.jsx
@@ -13,7 +13,7 @@ export default function LoginForm({ onSubmit, title, placeholder, buttonText}) {
     return (
         <form className="bg-white shadow-md max-w mx-auto p-5 mx-10 flex flex-col space-y-4" onSubmit={handleSubmit}>
             <h1 className="text-2xl font-bold text-center text-gray-700">{title}</h1>
-            <TextInput placeholder={placeholder} value={value} onChange={(e) => setValue(e.target.value)} name="team-id"/>
+            <TextInput inputmode="numeric" placeholder={placeholder} value={value} onChange={(e) => setValue(e.target.value)} name="team-id"/>
             <Button type="submit">{buttonText}</Button>
         </form>
     )
